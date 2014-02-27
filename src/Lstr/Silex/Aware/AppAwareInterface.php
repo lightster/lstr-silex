999b1c816f1f24feda6f70e1aab12d94edcb5a04
Matt Light <matt.light@lightdatasys.com>
2014-02-27 11:42:19 -0800

    Add AppAwareInterface for making objects aware of Silex DI

---
+++ src/Lstr/Silex/Aware/AppAwareInterface.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Lstr/Silex source code
+ *
+ * Copyright Matt Light <matt.light@lightdatasys.com>
+ *
+ * For copyright and licensing information, please view the LICENSE
+ * that is distributed with this source code.
+ */
+
+namespace Lstr\Silex\Aware;
+
+use Silex\Application;
+
+interface AppAwareInterface
+{
+    public function setSilexApplication(Application $app);
+    public function getSilexApplication(Application $app);
+}
